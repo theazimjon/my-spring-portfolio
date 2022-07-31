546aa2286212bcd0489bbbacd637bd4cd372d036
umarov_a <theazimjon@gmail.com>
2022-07-31 15:08:45 +0500

    ok

---
+++ src/test/java/com/qwasar/myspringportfolio/MySpringPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwasar.myspringportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
